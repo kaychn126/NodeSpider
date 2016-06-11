85edd25267aa629fdc1f8289be6ff51803c62cb9
陈凯 <1131472324@qq.com>
2016-06-11 19:51:17 +0800

    change

--- router.js
+++ router.js
@@ -1,10 +1,36 @@
 /**
  * Created by chenkai on 16/6/5.
  */
+var config = require("./supportingFiles/config");
+var dbConfig = new config.dbConfig();
+var mysql = require("mysql");
+var url = require("url");
+var guid = require("./spidertool");
+
+var pool = mysql.createPool({
+    host: dbConfig.dbhost,
+    user: dbConfig.dbuser,
+    password: dbConfig.dbpassword,
+    database: dbConfig.database
+});
+
 function route(pathname, handle, request, response){
     if (typeof handle[pathname] === 'function') {
         var openuuid = request.headers['openuuid'];
-        console.log('openuuid:' + openuuid);
+        pool.getConnection(function(err, connection){
+            connection.query('select * from User where openudid=?', openuuid, function(err1, rows){
+                if (!err1 && (rows===null || rows.length==0)) {
+                    var user = {};
+                    user.userId = guid.guid();
+                    user.openudid = openuuid;
+                    connection.query('insert into User set ?', user, function(err2){
+                        if (err2) {
+                            console.log(err2.message);
+                        }
+                    });
+                }
+            });
+        });
         handle[pathname](request, response);
     } else {
         console.log("No request handler found for " + pathname);
